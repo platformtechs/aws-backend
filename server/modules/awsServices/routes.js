f19bd59cb4d1baf698f6610fa9e9a6aa18a56973
tanishaaggarwal2502@gmail.com <tanishaaggarwal2502@gmail.com>
2019-08-19 21:04:59 +0530

    new aws-backend api

--- server/modules/awsServices/routes.js
+++ server/modules/awsServices/routes.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
 import * as AwsController from './controller';
 // import checkAuth from '../../config/checkAuth';
-import { upload } from '../../config/fileUpload';
+// import { upload } from '../../config/fileUpload';
 
 const routes = new Router();
 
-routes.post('/aws/create', AwsController.createUser);
-routes.post('/aws/update/:id', upload.single(), AwsController.updateUser);
-routes.post('/aws/delete', AwsController.deleteUser);
-routes.get('/aws/all', AwsController.getAllUser);
-routes.get('/aws/:id', AwsController.getUser);
+routes.post('/aws/create', AwsController.createInstance);
+routes.post('/aws/start', AwsController.startandstopInstance);
+routes.post('/aws/stop', AwsController.startandstopInstance);
+routes.get('/aws/reboot', AwsController.rebootInstance);
 export default routes;
