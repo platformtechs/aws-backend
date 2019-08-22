62cab0ff2279da6e84b4f4ba17f1059f06cb6788
tanishaaggarwal2502@gmail.com <tanishaaggarwal2502@gmail.com>
2019-08-22 11:25:05 +0530

    New User Routes added

--- server/modules/users/routes.js
+++ server/modules/users/routes.js
@@ -5,7 +5,10 @@ import { upload } from '../../config/fileUpload';
 
 const routes = new Router();
 
-routes.post('/user/create', UserController.createUser);
+routes.post('/user/create/user', UserController.createUser);
+routes.post('/user/create/admin', UserController.createAdmin);
+routes.post('/user/create/subadmin', UserController.createSubAdmin);
+routes.post('/user/create/accesskey', UserController.createAccesskey);
 routes.post('/user/login', UserController.login);
 routes.post('/user/update/:id', upload.single(), UserController.updateUser);
 routes.post('/user/delete', UserController.deleteUser);
